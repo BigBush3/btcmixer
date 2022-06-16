afb27fc4f12f48fe07c908576bbf5dcf2c86a1ab
Andrew <101003794+anreszii@users.noreply.github.com>
2022-06-16 21:13:07 +0300

    calc

--- front/components/MixerForm/components/FieldElement/InputAddress.js
+++ front/components/MixerForm/components/FieldElement/InputAddress.js
@@ -1,7 +1,7 @@
 import { PropTypes } from "prop-types";
 import { useState } from "react";
 
-export const InputAddress = ({ value }) => {
+export const InputAddress = ({ id }) => {
   return (
     <div className="field__element">
       <input
@@ -10,6 +10,28 @@ export const InputAddress = ({ value }) => {
         className="input-address"
         placeholder="1njrRcKQtfjjLuQxFYCeMXcth77m5TAYo"
         autoComplete="off"
+        onChange={(e) => {
+          if (id === 0) {
+            localStorage.setItem("address", e.target.value)
+            console.log("xuy")
+          }
+          if (id === 1) {
+            localStorage.setItem("address1", e.target.value)
+            console.log("xuy1")
+          }
+          if (id === 2) {
+            localStorage.setItem("address2", e.target.value)
+            console.log("xuy2")
+          }
+          if (id === 3) {
+            localStorage.setItem("address3", e.target.value)
+            console.log("xuy3")
+          }
+          if (id === 4) {
+            localStorage.setItem("address4", e.target.value)
+            console.log("xuy4")
+          }
+        }}
       />
     </div>
   );
